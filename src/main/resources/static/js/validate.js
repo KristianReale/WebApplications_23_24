7a908babd6843d6296f3b1c896b02921fa20493e
Kristian <kristian.reale@gmail.com>
2023-10-26 12:55:01 +0200

    validazione campi

--- src/main/resources/static/js/validate.js
+++ src/main/resources/static/js/validate.js
@@ -15,6 +15,40 @@ function verificaRistorante(){
     }
 }
 
+function aggiungiRistorante(){
+    var nomeRistTxt = document.getElementById("nome_rist");
+    var descRistTxt = document.getElementById("desc_rist");
+    var ubRistElement = document.getElementById("ub_ristorante");
+
+    var nomeRist = nomeRistTxt.value.trim();
+    var descRist = descRistTxt.value.trim();
+    var ubRist = ubRistElement.value.trim();
+
+    if (validateRistorante(nomeRist, descRist, ubRist)){
+        let newTr = document.createElement("tr");
+
+        let newTdNome = document.createElement("td");
+        let newTdDescrizione = document.createElement("td");
+        let newTdUbicazione = document.createElement("td");
+
+        let contentNome = document.createTextNode(nomeRist);
+        let contentDescrizione = document.createTextNode(descRist);
+        let contentUbicazione = document.createTextNode(contentDescrizione);
+
+        newTr.appendChild(newTdNome);
+        newTr.appendChild(newTdDescrizione);
+        newTr.appendChild(newTdUbicazione);
+
+        newTdNome.appendChild(contentNome);
+        newTdDescrizione.appendChild(contentDescrizione);
+        newTdUbicazione.appendChild(contentUbicazione);
+
+        let tableElement = document.getElementsByTagName("table")[0];
+        tableElement.appendChild(newTrjjj);
+    }else{
+        alert("Mancano dei campi");
+    }
+}
 function validateRistorante(nome, descrizione, ubicazione){
     var validationOK = true;
     if (nome == ""){
