6b57e296f23f1058085ea0bfdb927ce71833ac49
Kristian <kristian.reale@gmail.com>
2023-10-26 12:15:11 +0200

    introducing js

---
+++ src/main/resources/static/js/validate.js
@@ -0,0 +1,10 @@
+function verificaRistorante(){
+    var nomeRistTxt = document.getElementById("nome_rist");
+    var descRistTxt = document.getElementById("desc_rist");
+    var ubRistElement = document.getElementById("ub_ristorante");
+
+
+    alert(nomeRistTxt.value);
+    alert(descRistTxt.value);
+    alert(ubRistElement.value);
+}
