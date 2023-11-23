f3d4801c4ec8d2ce4e07f5f8822a51ca46c98a3e
Kristian <kristian.reale@gmail.com>
2023-11-23 11:56:43 +0100

    login

---
+++ src/main/java/it/unical/informatica/webapp24/recensioniristoranti/controller/servlet/LoginServlet.java
@@ -0,0 +1,20 @@
+package it.unical.informatica.webapp24.recensioniristoranti.controller.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/doLogin")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+    }
+}
