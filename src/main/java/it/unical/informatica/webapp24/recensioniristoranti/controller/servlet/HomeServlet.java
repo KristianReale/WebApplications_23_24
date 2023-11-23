bbd65ed92b72b77043fcac0a2ec050019e22d335
Kristian <kristian.reale@gmail.com>
2023-11-23 12:41:47 +0100

    login

---
+++ src/main/java/it/unical/informatica/webapp24/recensioniristoranti/controller/servlet/HomeServlet.java
@@ -0,0 +1,19 @@
+package it.unical.informatica.webapp24.recensioniristoranti.controller.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("views/index.html");
+        dispatcher.forward(req, resp);
+    }
+}
