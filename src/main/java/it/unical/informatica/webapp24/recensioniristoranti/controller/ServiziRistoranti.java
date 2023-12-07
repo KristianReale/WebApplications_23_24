c3ab5b95fb20c9d44469b3ea9a74ac0fcc8d42a5
Kristian <kristian.reale@gmail.com>
2023-12-07 13:19:18 +0100

    servizi ristorante

--- src/main/java/it/unical/informatica/webapp24/recensioniristoranti/controller/ServiziRistoranti.java
+++ src/main/java/it/unical/informatica/webapp24/recensioniristoranti/controller/ServiziRistoranti.java
@@ -2,12 +2,14 @@ package it.unical.informatica.webapp24.recensioniristoranti.controller;
 
 import it.unical.informatica.webapp24.recensioniristoranti.persistenza.DBManager;
 import it.unical.informatica.webapp24.recensioniristoranti.persistenza.model.Ristorante;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:4200/")
 public class ServiziRistoranti {
     @GetMapping("/ristorantiMigliori")
     public List<Ristorante> dammiRistorantiMigliori(){
